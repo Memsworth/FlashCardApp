00d15f4286cd375adfd90e93433dc8633a097ffe
Abubakar Ahmed <abubakarahmed4497@outlook.com>
2022-12-25 02:23:25 +0900

    added methods to controllers

--- FlashCardApp/FlashCardController.cs
+++ FlashCardApp/FlashCardController.cs
@@ -1,12 +1,19 @@
 ﻿namespace FlashCardApp;
-
 public class FlashCardController
 {
-    
     public FlashCardModel FlashCard { get; private set; }
-
     public FlashCardController(FlashCardModel flashCard)
     {
         FlashCard = flashCard;
+    }
+
+    public FlashCardModel CreateCard()
+    {
+        return new FlashCardModel("1", "1");
+    }
+
+    public FlashCardModel EditFlashCard(FlashCardModel model)
+    {
+        return model;
     }
 }
--- FlashCardApp/LanguageStackController.cs
+++ FlashCardApp/LanguageStackController.cs
@@ -2,5 +2,23 @@
 
 public class LanguageStackController
 {
-    
+    public FlashCardController FlashCardController { get; }
+    public LanguageStackController()
+    {
+        FlashCardController = new FlashCardController(new FlashCardModel("1" ,"1"));
+    }
+    public void CreateFlashCard()
+    {
+        var item = FlashCardController.CreateCard();
+        //TODO: insert flashcard into stack 
+    }
+    public void EditFlashCard(FlashCardModel model)
+    {
+        var item = FlashCardController.EditFlashCard(model);
+        //TODO: change model place with item and insert back
+    }
+    public void DeleteFlashCard(int id)
+    {
+        //TODO: Where id is equal quenry, delete
+    }
 }
